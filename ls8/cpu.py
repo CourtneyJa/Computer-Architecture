a973f4318d3078896a00b1bbd0da0f54ead36c37
CourtneyJa <courtney.jackson08234@gmail.com>
2020-10-05 23:31:56 -0400

    Day 2 tested and working

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -5,7 +5,7 @@ import sys
 HLT = 0b00000001
 LDI = 0b10000010
 PRN = 0b01000111
-
+MUL = 0b10100010
 
 class CPU:
     """Main CPU class."""
@@ -22,23 +22,24 @@ class CPU:
     def load(self):
         """Load a program into memory."""
 
-        address = 0
-
-        # For now, we've just hardcoded a program:
-
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
-
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
+        try:
+            address = 0
+            with open(sys.argv[1]) as file:
+                for line in file:
+                    split_it= line.split("#")
+                    value = split_it[0].strip()
+                    if value == "":
+                        continue
+                    try:
+                        instruction = int(value, 2)
+                    except ValueError:
+                        print(f"Invalid number '{value}'")
+                        sys.exit(1)
+                    self.ram[address] = instruction
+                    address += 1
+        except FileNotFoundError:
+            print(f"{sys.argv[0]} {sys.argv[1]} file not found")
+            sys.exit()
 
 
     def alu(self, op, reg_a, reg_b):
@@ -50,11 +51,11 @@ class CPU:
         else:
             raise Exception("Unsupported ALU operation")
     
-    def ram_read(self, key):
-        return self.ram[key]
+    def ram_read(self, address):
+        return self.ram[address]
 
-    def ram_write(self, key, value):
-        self.reg[key] = value
+    def ram_write(self, address, value):
+        self.reg[address] = value
 
     def trace(self):
         """
@@ -81,20 +82,25 @@ class CPU:
         self.running = True
                 
         while not self.halted:
-            instr_execute = self.ram_read(self.pc)
-            reg_idx_1 = self.memory(self.pc + 1)
-            reg_idx_2 = self.memory(self.pc + 2)
-            self. execute_instruction(instr_execute, reg_idx_1, reg_idx_2)
+            instruction = self.ram_read(self.pc)
+            reg_idx_1 = self.ram_read(self.pc + 1)
+            reg_idx_2 = self.ram_read(self.pc + 2)
+            self. execute_instruction(instruction, reg_idx_1, reg_idx_2)
 
     def execute_instruction(self, command, reg_idx_1, reg_idx_2):
         if command == HLT:
            self.halted = True
            self.pc += 1
-        elif command == LDI:
-            self.reg[reg_idx_1] = reg_idx_2
-            self.pc += 3
         elif command == PRN:
             print(self.reg[reg_idx_1])
             self.pc += 2
+        elif command == LDI:
+            self.reg[reg_idx_1] = reg_idx_2
+            self.pc += 3
+        elif command == MUL:
+            reg_a = self.ram_read(self.pc + 1)
+            reg_b = self.ram_read(self.pc + 2)
+            self.reg[reg_a] = self.reg[reg_a] * self.reg[reg_b]
+            self.pc += 3
 
         
